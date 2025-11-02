3e3426bf2ef412ad3b52ad01fbe100e52d3a91be
aruwksc <240966@astanait.edu.kz>
2025-11-02 14:37:29 +0500

    feat: add Graph and GraphBuilder classes for directed graph representation and construction

---
+++ src/main/java/aitu/edu/graph/util/PathReconstructor.java
@@ -0,0 +1,22 @@
+package aitu.edu.graph.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class PathReconstructor {
+
+    public static List<Integer> reconstruct(Map<Integer, Integer> pred, int src, int dst) {
+        List<Integer> rev = new ArrayList<>();
+        Integer cur = dst;
+        while (cur != null && cur != src) {
+            rev.add(cur);
+            cur = pred.get(cur);
+        }
+        if (cur == null) return Collections.emptyList();
+        rev.add(src);
+        Collections.reverse(rev);
+        return rev;
+    }
+}
